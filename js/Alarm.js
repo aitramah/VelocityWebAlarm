7d135de8a2bd3e60016b9f1b84ca142589dd4867
Colin Thompson <colinthompson@Colins-MacBook-Pro.local>
2017-03-22 13:19:37 -0600

    Basic Alarm Object Implementation

--- js/Alarm.js
+++ js/Alarm.js
@@ -2,26 +2,60 @@
  * Created by colinthompson on 2017-03-21.
  */
 
-
 function Alarm(daysOfWeek, hour, min, frequency, name) {
-    this.daysOfWeek = daysOfWeek; //A bitmask containing the days of the week the alarm should go off on
-    this.hour = hour; //The hour of the day to go off
-    this.min = min; //The minute to go off
-    this.frequency = frequency; //How frequently the alarm goes off (once, daily, weekly) (if daily bitmask should be all ones)
-    this.name = name; //The name of the alarm
+    var uuid = generateUUID();
+    var daysOfWeek = daysOfWeek; //A bitmask containing the days of the week the alarm should go off on
+    var hour = hour; //The hour of the day to go off
+    var min = min; //The minute to go off
+    var frequency = frequency; //How frequently the alarm goes off (once, daily, weekly) (if daily bitmask should be all ones)
+    var name = name; //The name of the alarm
+
+
+    Alarm.prototype.getDaysOfWeek = function () {
+        return daysOfWeek.slice(0);
+    };
+
+    Alarm.prototype.setDaysOfWeek = function (theDaysOfWeek) {
+        daysOfWeek = theDaysOfWeek;
+    };
+
+    Alarm.prototype.getName = function () {
+        return name;
+    };
+
+    Alarm.prototype.resetName = function (newName) {
+        name = newName;
+    };
+
+    Alarm.prototype.setHour = function (anHour) {
+        hour = anHour;
+    };
+
+    Alarm.prototype.getHour = function () {
+        return hour;
+    };
+
+    Alarm.prototype.setMinute = function (minute) {
+        min = minute;
+    };
+
+    Alarm.prototype.getMinute = function () {
+        return min;
+    };
+
+    Alarm.prototype.getFreq = function () {
+        return frequency;
+    };
+
+    Alarm.prototype.setFreq = function (freq) {
+        frequency = freq;
+    };
+
 }
 
-Alarm.prototype.getName = function() {
-    return this.name;
-}
+var AlarmFrequency = {
+    ONCE: 0,
+    DAILY: 1,
+    WEEKLY: 2,
+};
 
-Alarm.prototype.id = "test";
-
-
-var bob = new Alarm(1, 1, 1, 1, 1);
-var bill = new Alarm(1, 1, 1, 1, 1);
-
-console.log(bill.id);
-bob.id = "newid";
-console.log((bill.id));
-console.log((bob.id));
