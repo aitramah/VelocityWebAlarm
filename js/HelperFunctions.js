f3ad4a0e1299034a06093c531b1c03cc94cbf15c
danielVelasc <velasc.daniel@gmail.com>
2017-03-27 12:49:42 -0600

    Moved javascript functionality to an appropriate file (HelperFunctions.js)

--- js/HelperFunctions.js
+++ js/HelperFunctions.js
@@ -18,3 +18,63 @@ function generateUUID () { // Public Domain/MIT
         return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
     });
 }
+
+
+/**
+ * A general set up upon opening of the web app
+ */
+function setUp() {
+
+    /*
+     Hardcoded Alarm for Testing Purposes (Remove from Final Product)
+     */
+    var sampleDaysOfWeek = [true, false, false, false, false, false, false];
+    var sampleDate = new Date();
+    var sampleHour = sampleDate.getHours();
+    var sampleMin = sampleDate.getMinutes() + 1;
+    var sampleAlarm = new Alarm(sampleDaysOfWeek, sampleHour, sampleMin, AlarmFrequency.ONCE, "Wake up baby");
+
+    // Set Up AlarmCoordinator and Load Sample Alarm
+    var coordinator = new AlarmCoordinator();
+    coordinator.addNewAlarm(sampleAlarm);
+
+
+    /*
+     By: Daniel Velasco
+
+     The following function grabs alarms from the AlarmCoordinator, creates an HTML
+     div for each with appropriate information, then appends them to the proper
+     place in the index document
+
+     */
+    var alarmViews = document.getElementById('alarm-block');
+    var alarms = coordinator.getAlarms();
+
+    // For each alarm held in the coordinator, create a view
+    for(var i = 0; i < alarms.length; i++) {
+        // This is the overarching single alarm view
+        var singleAlarmViewDiv = document.createElement('div');
+        singleAlarmViewDiv.className = 'row';
+
+        // Creating the alarm name element
+        var alarmNameHeader = document.createElement('H3');
+        var alarmName = document.createTextNode(alarms[i].getName());
+        alarmNameHeader.appendChild(alarmName);
+
+        // Creating the alarm time element
+        var alarmTimeHeader = document.createElement('H1');
+        var alarmTime = document.createTextNode(alarms[i].getHour()+":"+alarms[i].getMinute())
+        alarmTimeHeader.appendChild(alarmTime);
+
+        // Creating the frequency element
+        var frequencyParagraph = document.createElement('p');
+        var frequencyText = document.createTextNode(alarms[i].getFreq());
+        frequencyParagraph.appendChild(frequencyText);
+
+        singleAlarmViewDiv.appendChild(alarmNameHeader);
+        singleAlarmViewDiv.appendChild(alarmTimeHeader);
+        singleAlarmViewDiv.appendChild(frequencyParagraph);
+
+        alarmViews.appendChild(singleAlarmViewDiv);
+    }
+}
