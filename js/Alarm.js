01891ace4632d9a8490b5900e2f410c2040034d4
aidanBailey <aidan.bailey@ucalgary.ca>
2017-03-26 20:54:36 -0600

    Added Alarm Functionality, although it does not work yet :(

--- js/Alarm.js
+++ js/Alarm.js
@@ -4,9 +4,9 @@
 
 function Alarm(daysOfWeek, hour, min, frequency, name) {
     var uuid = generateUUID();
-    var daysOfWeek = daysOfWeek; //A bitmask containing the days of the week the alarm should go off on
-    var hour = hour; //The hour of the day to go off
-    var min = min; //The minute to go off
+    var daysOfWeek = daysOfWeek; //A bitmask containing the days of the week the alarm should go off on (array of booleans)
+    var hour = hour; //The hour of the day to go off (0 - 23)
+    var min = min; //The minute to go off (0 - 59)
     var frequency = frequency; //How frequently the alarm goes off (once, daily, weekly) (if daily bitmask should be all ones)
     var name = name; //The name of the alarm
 
--- js/AlarmCoordinator.js
+++ js/AlarmCoordinator.js
@@ -1,7 +1,60 @@
 /**
+ * Intended to check alarms to see if they are ready to go off.
  * Created by Aidan on 2017-03-26.
  */
+function AlarmCoordinator(){
+    var alarmList = [];
 
-function checkAlarms() {
+    AlarmCoordinator.prototype.addNewAlarm = function(alarm){
+        alarmList.push(alarm);
+    }
 
+    AlarmCoordinator.prototype.checkAlarms = function() {
+        alert("Testing 1");
+        var alarmLength = alarmList.length;
+        var today = new Date();
+        var newArray = [];
+
+        // Current Time Variables
+        var h = today.getHours();
+        var m = today.getMinutes();
+        var weekday = today.getDay();
+
+        for(i = 0; i < alarmLength; i++){
+            var tempAlarm = alarmList[i];
+
+            // Alarm Variables
+            var alarmDays = tempAlarm.getDaysOfWeek();
+            var alarmHour = tempAlarm.getHour();
+            var alarmMinute = tempAlarm.getMinute();
+            var alarmFrequency = tempAlarm.getFreq();
+
+            if(!alarmDays[weekday] || alarmHour != h) {
+                newArray.push(tempAlarm);
+                continue;
+            }
+            else if(m == alarmMinute){
+                if(alarmFrequency > 0){
+                    newArray.push(tempAlarm);
+                }
+                this.alarmGoingOff(i);
+            }
+            else{
+                newArray.push(tempAlarm);
+            }
+        }
+
+        // Re-assign the Array of Alarms to remove any alarms that have gone off
+        // and shouldn't go off again
+        alarmList = newArray;
+
+        // Restart the Function and check again
+        setTimeout(this.checkAlarms, 5000); // Restart every 5 seconds
+    }
+
+    AlarmCoordinator.prototype.alarmGoingOff = function(index){
+        var tempAlarm = alarmList[i];
+        var alarmName = tempAlarm.getName();
+        alert("Alarm Going Off: " + alarmName);
+    }
 }
